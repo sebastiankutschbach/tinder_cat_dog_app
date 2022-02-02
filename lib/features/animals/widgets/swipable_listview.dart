be7a977851f2cf6ae2550cb91601583d0ba49068
sebastiankutschbach <sebastian.kutschbach@freenet.de>
2022-02-02 19:30:58 +0100

    avoid index overflow for swipable listview

--- lib/features/animals/widgets/swipable_listview.dart
+++ lib/features/animals/widgets/swipable_listview.dart
@@ -43,7 +43,7 @@ class SwipableListview extends StatelessWidget {
             ),
           ),
           onDismissed: (direction) {
-            onLikedItem(index, direction == DismissDirection.startToEnd);
+            onLikedItem(index % itemCount, direction == DismissDirection.startToEnd);
           },
         ),
       ),
